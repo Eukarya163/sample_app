697b3fe52d417ae0827dcbb05cabdefb2b19a11e
Eukarya163 <180396448+Eukarya163@users.noreply.github.com>
2024-10-01 13:06:17 +0000

    モデル「List」を作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "4ad53a867a354178bf2b4e822ea4686a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
