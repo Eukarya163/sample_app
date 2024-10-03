4f099cf3801ebb395a66f39095b941497d145a67
Eukarya163 <180396448+Eukarya163@users.noreply.github.com>
2024-10-03 11:00:28 +0000

    リダイレクトで/top に移動しなかったのを直した

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,7 @@ class ListsController < ApplicationController
     # インスタンス変数(@付き)はViewファイルへ受け渡し可能
     # ローカル変数は不可能(アクション内のみ利用可能)
     list = List.new(list_params)
-    # データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # トップ画面へリダイレクト
     redirect_to '/top'
   end
   
