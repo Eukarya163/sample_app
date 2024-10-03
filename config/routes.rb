24334387d9a4796b0bd8054418e3d300d698dfb2
Eukarya163 <180396448+Eukarya163@users.noreply.github.com>
2024-10-03 11:59:23 +0000

    show ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :lists
   get 'lists/new'
   get 'lists' => 'lists#create'
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
