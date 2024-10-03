da59408317a248a3d59dff1bc9903b0555564fb8
Eukarya163 <180396448+Eukarya163@users.noreply.github.com>
2024-10-03 12:34:34 +0000

    [finish]アプリケーションを完成させよう5章

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :lists
   get 'lists/new'
-  get 'lists' => 'lists#create'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'home#top'
+  post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
-  get '/top' => 'homes#top'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
